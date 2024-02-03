82966534c8f6e2d05adb2590b71e4e72d844d306
Nikita Vashkulatov <nikitavaskulatov@gmail.com>
2024-02-03 14:10:55 +0000

    smth

--- codest-shared/src/main/kotlin/r3nny/codest/shared/domain/TestType.kt
+++
@@ -1,7 +0,0 @@
-package r3nny.codest.shared.domain
-
-enum class TestType(val description: String) {
-    DEFAULT("Точное совпадение"),
-    ANY("Любое из"),
-    ANY_ORDER("Любой порядок массива")
-}
