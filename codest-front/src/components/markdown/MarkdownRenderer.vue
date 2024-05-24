78dc6a4c08fad261b5c6316a42a36761fd4108f8
Nikita Vashkulatov <vashkulatov.nikita@promregion.net>
2024-05-24 20:55:57 +0300

    tmp

---
+++ codest-front/src/components/markdown/MarkdownRenderer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-html="markdown.render(source)" />
+</template>
+
+<script setup>
+import MarkdownIt from "markdown-it";
+
+const markdown = new MarkdownIt()
+
+defineProps({
+  source: {
+    type: String,
+    default: ""
+  }
+});
+</script>
+
+<style scoped>
+div {
+  border: 2px solid var(--color-border);
+  border-radius: 5px;
+  padding: 10px;
+}
+</style>
